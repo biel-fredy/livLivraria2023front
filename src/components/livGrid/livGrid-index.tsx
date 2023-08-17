c2aa70bda91abf02b8c3bec0ef48d8f76882dfe2
Gabriel Frederico Barbosa <biel1801fredy@gmail.com>
2023-08-17 01:37:35 -0300

    feat: adicionado filtros no grid

--- src/components/livGrid/livGrid-index.tsx
+++ src/components/livGrid/livGrid-index.tsx
@@ -1,5 +1,5 @@
 // livGrid-index.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Table,
   TableContainer,
@@ -8,9 +8,13 @@ import {
   TableRow,
   TableCell,
   Paper,
+  Input,
+  IconButton,
 } from '@mui/material';
-import { GridContainerStyle, BoldTableCellStyle } from './livGrid-styles'; // Importe os estilos
+
+import { GridContainerStyle, BoldTableCellStyle } from './livGrid-styles';
 import Quadro from '../quadro/quadro-index';
+import { Close, FilterList } from '@mui/icons-material';
 
 interface LivGridProps {
   data: any[];
@@ -18,28 +22,72 @@ interface LivGridProps {
 
 const LivGrid: React.FC<LivGridProps> = ({ data }) => {
   const columns = Object.keys(data[0] || {});
+  const [filters, setFilters] = useState<{ [key: string]: string }>({});
+  const [editingColumn, setEditingColumn] = useState<string | null>(null);
+
   const capitalizeFirstLetter = (string: string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
+  const handleFilterChange = (column: string, value: string) => {
+    setFilters({ ...filters, [column]: value });
+  };
+
+  const handleEditClick = (column: string) => {
+    setEditingColumn(column);
+  };
+
+  const handleCancelFilter = (column: string) => {
+    const newFilters = { ...filters };
+    delete newFilters[column];
+    setFilters(newFilters);
+    setEditingColumn(null);
+  };
+
+  const filteredData = data.filter((row) => {
+    return Object.keys(filters).every((column) => {
+      return filters[column]
+        ? row[column].toString().includes(filters[column])
+        : true;
+    });
+  });
+
   return (
     <Quadro titulo="Grid">
       <div style={GridContainerStyle}>
-        {' '}
-        {/* Aplicar estilo aqui */}
         <TableContainer component={Paper}>
           <Table>
             <TableHead>
               <TableRow>
                 {columns.map((column) => (
                   <TableCell key={column} style={BoldTableCellStyle}>
-                    {capitalizeFirstLetter(column)}
+                    {editingColumn === column ? (
+                      <>
+                        <Input
+                          defaultValue={filters[column] || ''}
+                          onChange={(e) =>
+                            handleFilterChange(column, e.target.value)
+                          }
+                          autoFocus
+                        />
+                        <IconButton onClick={() => handleCancelFilter(column)}>
+                          <Close />
+                        </IconButton>
+                      </>
+                    ) : (
+                      <>
+                        {capitalizeFirstLetter(column)}
+                        <IconButton onClick={() => handleEditClick(column)}>
+                          <FilterList />
+                        </IconButton>
+                      </>
+                    )}
                   </TableCell>
                 ))}
               </TableRow>
             </TableHead>
             <TableBody>
-              {data.map((row, index) => (
+              {filteredData.map((row, index) => (
                 <TableRow key={index}>
                   {columns.map((column) => (
                     <TableCell key={column}>{row[column]}</TableCell>
